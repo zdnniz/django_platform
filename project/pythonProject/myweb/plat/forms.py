c73259b9eb13994c8ccfef3b61152799ee2199af
zdnniz <zdennis076@gmail.com>
2024-03-03 01:35:45 -0800

    1

--- project/pythonProject/myweb/plat/forms.py
+++ project/pythonProject/myweb/plat/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import ImportCsv
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
