8c1174d51c42c54bf58a1d5f6634b7340477d8ff
zdnniz <zdennis076@gmail.com>
2024-03-13 20:27:27 -0700

    git commit

--- project/pythonProject/myweb/myweb/settings.py
+++ project/pythonProject/myweb/myweb/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'plat',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- project/pythonProject/myweb/plat/admin.py
+++ project/pythonProject/myweb/plat/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ImportCsv
+'''from .models import ImportCsv
 
-admin.register(ImportCsv)
+admin.register(ImportCsv)'''
