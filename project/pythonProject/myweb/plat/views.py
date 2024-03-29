00b1ea183ae38903cece645afcbc3e16af398200
zdnniz <zdennis076@gmail.com>
2024-03-28 23:21:45 -0700

    git commit

--- project/pythonProject/myweb/plat/views.py
+++ project/pythonProject/myweb/plat/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from django.core.paginator import Paginator
+
+from pyecharts.charts import Pie, Bar
+
 from .forms import RegisterForm
 from .models import *
+
+from .GCN_AnomalyDetection_pytorch.test import train_dominant
 # Create your views here.
 
 def home(request):
@@ -22,8 +26,6 @@ def home(request):
 
     datasets = Dataset.objects.all()
     selected_dataset_id = request.GET.get('dataset')
-    page_number = request.GET.get('page', 1)  # 获取当前页码，默认为第一页
-    per_page = 5  # 每页显示的项目数量
 
     if selected_dataset_id == "00001":
         selected_dataset = ImportIEEE.objects.filter(isFraud=1)
@@ -32,25 +34,53 @@ def home(request):
 
     elif selected_dataset_id == "00002":
         try:
-            selected_dataset = ImportIEEE.objects.filter(isFraud=1)
+            import argparse
+
+            dataset_name = 'BlogCatalog'
+            hidden_dim = 64
+            epoch = 100
+            lr = 5e-3
+            dropout = 0.3
+            alpha = 0.8
+            device = 'cpu'
+
+            fake_args = argparse.Namespace(
+                dataset=dataset_name,
+                hidden_dim=hidden_dim,
+                epoch=epoch,
+                lr=lr,
+                dropout=dropout,
+                alpha=alpha,
+                device=device
+            )
+
+            train_dominant(fake_args)
+
+            gcn_dataset = train_dominant(fake_args)
+
+            n = len(gcn_dataset)
+            gcn_label = ['labels'] * n
+
+            # select_dataset = dict(zip(gcn_label, gcn_dataset))
+            select_dataset = gcn_dataset.tolist()
 
         except Dataset.DoesNotExist:
             messages.error(request, "所选数据集不存在。")
 
-        return render(request, 'home.html', {'datasets': datasets, 'selected_dataset': selected_dataset})
+        return render(request, 'home.html', {'datasets': datasets, 'select_dataset': select_dataset})
 
     else:
         selected_dataset = ImportIEEE.objects.none()
 
-
-
     return render(request, 'home.html', {'datasets': datasets, 'selected_dataset': selected_dataset})
+
 
 def logout_view(request):
     logout(request)
     messages.success(request, "OUT")
     print("out")
     return redirect('home')
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -66,18 +96,16 @@ def register_view(request):
             login(request, user)
 
             return redirect('home')
-        
+
     else:
         form = RegisterForm()
-        
-        return render(request, 'register.html', {'form':form})
-        
-    return render(request, 'register.html', {'form':form})
+
+        return render(request, 'register.html', {'form': form})
+
+    return render(request, 'register.html', {'form': form})
+
 
 def charts_view(request):
-    from pyecharts.charts import Pie, Bar
-    from pyecharts import options as opts
-
     model = ImportIEEE.objects.filter()
 
     fraud_number = 0
